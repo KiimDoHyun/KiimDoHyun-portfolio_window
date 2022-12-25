f8f863d9553bf4f0686dc724e56e7ee9c32b2c23
KiimDoHyun <bzidol@naver.com>
2022-12-26 01:31:03 +0900

    회전 설정값 수정

--- src/Container/Program/ImageProgramContainer.jsx
+++ src/Container/Program/ImageProgramContainer.jsx
@@ -52,12 +52,12 @@ const ImageProgramContainer = ({ type, parent, name }) => {
 
     // 이미지 회전_왼쪽
     const onClickM_Image_Rotate_Right = useCallback(() => {
-        setCurrentImage_rotate((prev) => (prev + 90) % 360);
+        setCurrentImage_rotate((prev) => (prev - 45) % 360);
     }, [setCurrentImage_sizeRate]);
 
     // 이미지 회전_오른쪽
     const onClickM_Image_Rotate_Left = useCallback(() => {
-        setCurrentImage_rotate((prev) => (prev - 90) % 360);
+        setCurrentImage_rotate((prev) => (prev + 45) % 360);
     }, [setCurrentImage_sizeRate]);
 
     // 이미지 기본 설정으로 변경
