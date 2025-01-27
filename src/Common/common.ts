f257beb84320681359ab9f06e5017f7ed108adce
김도현 <bzidol@naver.com>
2025-01-27 15:34:20 +0900

    fix(common): dateToStr 타입 지정

--- src/Common/common.ts
+++ src/Common/common.ts
@@ -1,56 +1,60 @@
-export const dateToStr = (date, format = "d", isHyphen = true) => {
-    const d = date instanceof Date ? date : new Date(date);
-    const _year = d.getFullYear();
-    const _month = ("0" + (d.getMonth() + 1)).slice(-2);
-    const _date = ("0" + d.getDate()).slice(-2);
-    const _hour = ("0" + d.getHours()).slice(-2);
-    const _minutes = ("0" + d.getMinutes()).slice(-2);
-    const _seconds = ("0" + d.getSeconds()).slice(-2);
+export const dateToStr = (
+  date: Date | number,
+  format = "d",
+  isHyphen = true
+) => {
+  const d = typeof date === "number" ? new Date(date) : date;
+  const _year = d.getFullYear();
+  const _month = ("0" + (d.getMonth() + 1)).slice(-2);
+  const _date = ("0" + d.getDate()).slice(-2);
+  const _hour = ("0" + d.getHours()).slice(-2);
+  const _minutes = ("0" + d.getMinutes()).slice(-2);
+  const _seconds = ("0" + d.getSeconds()).slice(-2);
 
-    switch (format) {
-        case "y":
-            if (isHyphen) {
-                return `${_year}`;
-            } else {
-                return `${_year}년`;
-            }
-        case "m":
-            if (isHyphen) {
-                return `${_year}-${_month}`;
-            } else {
-                return `${_year}년 ${_month}월`;
-            }
-        case "d":
-            if (isHyphen) {
-                return `${_year}-${_month}-${_date}`;
-            } else {
-                return `${_year}년 ${_month}월 ${_date}일`;
-            }
-        case "h":
-            if (isHyphen) {
-                return `${_year}-${_month}-${_date} ${_hour}`;
-            } else {
-                return `${_year}년 ${_month}월 ${_date}일 ${_hour}시`;
-            }
-        case "min":
-            if (isHyphen) {
-                return `${_year}-${_month}-${_date} ${_hour}:${_minutes}`;
-            } else {
-                return `${_year}년 ${_month}월 ${_date}일 ${_hour}시 ${_minutes}분`;
-            }
-        case "sec":
-            if (isHyphen) {
-                return `${_year}-${_month}-${_date} ${_hour}:${_minutes}:${_seconds}`;
-            } else {
-                return `${_year}년 ${_month}월 ${_date}일 ${_hour}시 ${_minutes}분 ${_seconds}초`;
-            }
-        case "time":
-            if (isHyphen) {
-                return `${_hour}:${_minutes}`;
-            } else {
-                return `${_hour}시 ${_minutes}분`;
-            }
-        default:
-            return d;
-    }
+  switch (format) {
+    case "y":
+      if (isHyphen) {
+        return `${_year}`;
+      } else {
+        return `${_year}년`;
+      }
+    case "m":
+      if (isHyphen) {
+        return `${_year}-${_month}`;
+      } else {
+        return `${_year}년 ${_month}월`;
+      }
+    case "d":
+      if (isHyphen) {
+        return `${_year}-${_month}-${_date}`;
+      } else {
+        return `${_year}년 ${_month}월 ${_date}일`;
+      }
+    case "h":
+      if (isHyphen) {
+        return `${_year}-${_month}-${_date} ${_hour}`;
+      } else {
+        return `${_year}년 ${_month}월 ${_date}일 ${_hour}시`;
+      }
+    case "min":
+      if (isHyphen) {
+        return `${_year}-${_month}-${_date} ${_hour}:${_minutes}`;
+      } else {
+        return `${_year}년 ${_month}월 ${_date}일 ${_hour}시 ${_minutes}분`;
+      }
+    case "sec":
+      if (isHyphen) {
+        return `${_year}-${_month}-${_date} ${_hour}:${_minutes}:${_seconds}`;
+      } else {
+        return `${_year}년 ${_month}월 ${_date}일 ${_hour}시 ${_minutes}분 ${_seconds}초`;
+      }
+    case "time":
+      if (isHyphen) {
+        return `${_hour}:${_minutes}`;
+      } else {
+        return `${_hour}시 ${_minutes}분`;
+      }
+    default:
+      return "Error";
+  }
 };
