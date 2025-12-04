bd3c28225f19e91971c8895a1b2e5c2c04120ba8
DOH <bzidol@naver.com>
2025-12-04 23:59:17 +0900

    feat(Login): 요소 위치 지정

--- src/fsd/window/3_widgets/Login/Login.tsx
+++ src/fsd/window/3_widgets/Login/Login.tsx
@@ -12,15 +12,33 @@ export default function Login() {
       <div
         className={flex({ direction: "column", width: "100%", height: "100%" })}
       >
-        <div className={css({ flex: 1 })}>
-          <div>
-            {hour}: {minute}
-          </div>
-          <div>
-            {month}월 {day}일 일요일
+        <div
+          className={flex({
+            flex: 1,
+            alignItems: "flex-end",
+          })}
+        >
+          <div
+            className={flex({
+              direction: "column",
+              width: "100%",
+              height: "100%",
+              alignItems: "flex-start",
+              justifyContent: "flex-end",
+              padding: 8,
+            })}
+          >
+            <h1>
+              {hour}:{minute}
+            </h1>
+            <h3>
+              {month}월 {day}일 일요일
+            </h3>
           </div>
         </div>
-        <div>
+        <div
+          className={flex({ justifyContent: "flex-end", padding: 4, gap: 2 })}
+        >
           <Icon />
           <Icon />
         </div>
