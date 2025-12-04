eaa20d83427b499b6f5c63a3b208b00bb896208b
DOH <bzidol@naver.com>
2025-12-04 23:42:35 +0900

    feat(Login): 기본 레이아웃 적용

--- src/fsd/window/3_widgets/Login/Login.tsx
+++ src/fsd/window/3_widgets/Login/Login.tsx
@@ -1,5 +1,42 @@
 import FullScreenBox from "@fsd/window/6_common/ui/FullScreenBox";
+import { css } from "@styled-system/css";
+import { flex } from "@styled-system/patterns";
 
 export default function Login() {
-  return <FullScreenBox>Login</FullScreenBox>;
+  const hour = 9;
+  const minute = 14;
+  const month = 9;
+  const day = 13;
+  return (
+    <FullScreenBox>
+      <div
+        className={flex({ direction: "column", width: "100%", height: "100%" })}
+      >
+        <div className={css({ flex: 1 })}>
+          <div>
+            {hour}: {minute}
+          </div>
+          <div>
+            {month}월 {day}일 일요일
+          </div>
+        </div>
+        <div>
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+    </FullScreenBox>
+  );
 }
+
+const Icon = () => {
+  return (
+    <div
+      className={css({
+        width: "20px",
+        height: "20px",
+        backgroundColor: "gray",
+      })}
+    />
+  );
+};
